f80b38f18c1134f7760fd8193616c0d9ff72fded
brenddamachado <brenda16machado.bm@gmail.com>
2023-04-20 23:00:30 -0300

    fim do jogo

--- jogo/src/components/gameover/GameOver.jsx
+++ jogo/src/components/gameover/GameOver.jsx
@@ -5,11 +5,13 @@ import WellDone from '../../Img/welldone.svg';
 import './GameOver.css'
 
 const GameOver = () => {
+  const [quizStart, dispatch]  = useContext(QuizContext)
   return (
     <div id='gameover'>
 <h2>Fim de jogo</h2>
-<p>Pontuação; x</p>
-<p>Você acertou y de z perguntas</p>
+<p>Pontuação: {quizStart.score}</p>
+<p>Você acertou {quizStart.score} de {quizStart.questions.length} 
+{''} perguntas.</p>
 <img src={WellDone} alt="fim do jogo" />
 <button> Reiniciar</button>
     </div>
