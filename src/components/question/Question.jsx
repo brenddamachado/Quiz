d08c6565799413de048b7416f1f3e958c0d962e9
brenddamachado <brenda16machado.bm@gmail.com>
2023-04-21 14:39:32 -0300

    'atualização'

---
+++ src/components/question/Question.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useContext } from 'react'
+import { QuizContext } from '../../context/Quiz'
+import './Question.css'
+import Option from '../option/Option'
+
+
+
+export const Question = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+  const currentQuestion = quizState.questions[quizState.currentQuestion];
+  const onSelectOption = (option) => {
+    dispatch({
+      type: 'CHECK_ANSWER',
+      payload: { answer: currentQuestion.answer, option }
+    })
+  }
+
+  return (
+    <div id='question'>
+      <p>
+        Pegunta {quizState.currentQuestion + 1} de {quizState.questions.length}
+      </p>
+
+      <h2>{currentQuestion.question}</h2>
+      <div id="options-container">
+        {currentQuestion.options.map((option) => (
+          <Option
+            option={option}
+            key={option}
+            answer={currentQuestion.answer}
+            selectOption={() => onSelectOption(option)}
+
+          />
+        ))}
+      </div>
+      {quizState.answerSelected && (
+        <button onClick={() => dispatch({ type: "CHANGE_QUESTION" })}
+        >Continuar</button>
+      )}
+    </div>
+  )
+}
