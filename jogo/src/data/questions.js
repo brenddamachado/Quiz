44f152d19b7861c7491af65a46ab50775b592619
brenddamachado <brenda16machado.bm@gmail.com>
2023-04-21 14:15:18 -0300

    'atualização'

--- jogo/src/data/questions.js
+++ jogo/src/data/questions.js
@@ -30,6 +30,40 @@ const data = [
     options: ["qty", "length", "items", "index"],
     answer: "length",
   },
+  {
+    question: "Qual tag cria um parágrafo?",
+    options: ["<p>", "<h1>", "<text>", "<ul>"],
+    answer: "<p>",
+  },
+  {
+    question: "Qual atributo adiciona um link para a tag a?",
+    options: ["alt", "href", "src", "link"],
+    answer: "href",
+  
+  },
+  {
+    question: "A posição que deixa um elemento fixo é a:",
+    options: ["static", "absolute", "fixed", "relative"],
+    answer: "fixed",
+  }, {
+    question: "Para aumentar a fonte de um elemento utilizamos:",
+    options: ["font", "text-transform", "font-size", "hover"],
+    answer: "font-size",
+  }, {
+    question: "Qual regra altera a cor de um elemento?",
+    options: ["color", "background-color", "font-size", "transition"],
+    answer: "color",
+    tip: "Cor em inglês",
+  },{
+    question: "As listas não ordenadas tem a tag de:",
+    options: ["<ol>", "<ul>", "<li>", "<list>"],
+    answer: "<ul>",
+  },
+  {
+    question: "Qual atributo deixa o input obrigatório?",
+    options: ["placeholder", "value", "required", "maxlength"],
+    answer: "required",
+  },
 ];
 
 export default data;
